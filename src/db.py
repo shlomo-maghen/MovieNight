b69acdda43a6b51beaba8ded6f94df7f77bc1d2e
Shlomo <shlomoshlomoshlomo@gmail.com>
2024-08-27 11:25:22 -0400

    Initial commit

---
+++ src/db.py
@@ -0,0 +1,48 @@
+import re
+
+from pymongo import MongoClient
+from util import generate_room_id
+from util import convert_movie
+
+class DB():
+  
+  def __init__(self):
+    uri = "localhost:27017"
+    client = MongoClient(uri)
+    self.db = client.movienight
+
+  def create_room(self):
+    collection = self.db.rooms
+    room_id = generate_room_id()
+    result = collection.insert_one({'room_id' : room_id})    
+    return { "success" : True, "room_id" : result["room_id"] }
+
+  def all_rooms(self):
+    collection = self.db.rooms
+    rooms = collection.find()
+    return rooms
+  
+  def single_room(self, room_id):
+    collection = self.db.rooms
+    room = collection.find_one({"room_id": room_id})
+    return room
+  
+  def search_movies(self, term):
+    collection = self.db.movies
+    term = re.compile(term, re.IGNORECASE)
+    movies = collection.find({"title" : {"$regex" : term}})
+    movies = [convert_movie(movie) for movie in movies]
+    return movies
+  
+  def add_movie_to_room(self, room_id, movie_id, user_id):
+    rooms = self.db.rooms
+    rooms.update_one(
+      {"room_id" : room_id},
+      {"$addToSet": {"movies" : {"movie_id" : movie_id, "user_id" : user_id} }})
+      
+  def remove_movie_from_room(self, room_id, movie_id, user_id):
+    rooms = self.db.rooms
+    rooms.update_one(
+      {"room_id" : room_id},
+      {"$pull": {"movies" : { "movie_id" : movie_id, "user_id" : user_id}  }})
+    
---
+++ src/models.py
@@ -0,0 +1,11 @@
+class Movie:
+  
+  def __init__(self, title, id):
+    self.id = id
+    self.title = title
+  
+  def title(self):
+    return self.title
+  
+  def id(self):
+    return self.id
